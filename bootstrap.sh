fe9040ef21957b27f3a48e894de7cc36499988b1
Simon Brundin <simonbrundin@gmail.com>
2025-10-21 00:55:13 +0200

    bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing Devbox..."
+curl -fsSL https://get.jetify.com/devbox | bash
+
+echo "Installing Nushell via devbox global..."
+devbox global add nushell
+
+echo "Cloning Simon CLI from GitHub..."
+mkdir "$HOME/repos"
+git clone https://github.com/simon/simon-cli "$HOME/repos/simon-cli"
+
+echo "Running simon setup via nushell..."
+nu -c "$HOME/repos/simon-cli/simon setup"
