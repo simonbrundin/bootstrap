bc221a071b5fa3473022f2df17e0a000fdfd5130
Simon Brundin <simonbrundin@gmail.com>
2025-10-22 15:20:05 +0200

    okej

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 echo "version: 0.0.1"
 # TODO: Fråga efter användarns GitHub användarnamn
+read -r -p "Ange användarnamn till GitHub: " GITHUB_USERNAME
 
 # Konfigurera datorns dotfiles via Chezmoi
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply "$GITHUB_USERNAME"
 
-echo "Installing Devbox..."
-curl -fsSL https://get.jetify.com/devbox | bash
+# echo "Installing Devbox..."
+# curl -fsSL https://get.jetify.com/devbox | bash
+#
+# echo "Installing Nushell via devbox global..."
+# devbox global add nushell
 
-echo "Installing Nushell via devbox global..."
-devbox global add nushell
-
+# Skapa repos-mapp
 echo "Setting up repos directory..."
 mkdir "$HOME/repos"
 
+# Klona Simon CLI
 if [ -d "$HOME/repos/simon-cli" ]; then
   echo "Simon CLI directory exists, pulling latest changes..."
   cd "$HOME/repos/simon-cli" && git pull
@@ -22,5 +25,5 @@ else
   git clone https://github.com/simon/simon-cli "$HOME/repos/simon-cli"
 fi
 
-echo "Running simon bootstrap via nushell..."
-nu -c "$HOME/repos/simon-cli/simon bootstrap mac"
+# echo "Running simon bootstrap via nushell..."
+# nu -c "$HOME/repos/simon-cli/simon bootstrap mac"
